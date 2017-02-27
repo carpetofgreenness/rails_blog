dd239aacc0dcc2433646db19a547ba9dd98b161b
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-02-27 11:34:44 -0500

    resolved bug where new user can't post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 
-	validates_inclusion_of :user_id, in: User.all.map(&:id)
+
 	validates_presence_of :title, :body
 end
